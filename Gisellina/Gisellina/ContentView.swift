e92e6404f549743c16470b1d3ad8e78eb6dd5550
Jamin <jamin@10-141-43-10.0103.pos.kr.iosda.org>
2025-05-26 16:31:39 +0900

    [Add] Xcode Project

---
+++ Gisellina/Gisellina/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gisellina
+//
+//  Created by Jamin on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gisellina/Gisellina/GisellinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GisellinaApp.swift
+//  Gisellina
+//
+//  Created by Jamin on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct GisellinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
