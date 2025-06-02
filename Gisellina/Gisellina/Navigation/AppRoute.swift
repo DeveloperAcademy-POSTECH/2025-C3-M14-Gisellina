6357e1b4a78d0a3a3ae9336073aea65009623827
Jamin <jamin@10-141-43-22.0103.pos.kr.iosda.org>
2025-06-02 16:12:26 +0900

    [Feat] 화면 이동 위한 작업

---
+++ Gisellina/Gisellina/Navigation/AppRoute.swift
@@ -0,0 +1,14 @@
+//
+//  AppRoute.swift
+//  Gisellina
+//
+//  Created by Jamin on 6/2/25.
+//
+
+
+//MARK: - 우리가 갈 수 있는 목적지 모음
+enum AppRoute: Hashable {
+    case studyMission
+    case exerciseMission
+    case missionList
+}
