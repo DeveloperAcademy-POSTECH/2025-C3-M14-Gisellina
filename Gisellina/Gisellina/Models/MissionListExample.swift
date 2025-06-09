03f035a00af18715317afd7c26cd0c34687971c4
Jamin <jamin@10-141-48-63.0200.pos.kr.iosda.org>
2025-06-09 12:57:57 +0900

    [Feat] Noah를 위한 예시..

--- Gisellina/Gisellina/Models/MissionListExample.swift
+++ Gisellina/Gisellina/Models/MissionListExample.swift
@@ -1,7 +1,26 @@
 //
-//  MissionList.swift
+//  MissionListExample.swift
 //  Gisellina
 //
 //  Created by Jamin on 6/9/25.
 //
 
+struct MissionListExample: Decodable, Identifiable {
+    let id: String
+    let userId: String
+    
+    let missionDetails: MissionDetail
+    
+    // View에서 보여줘야할 정보들
+    let createdAt: String
+    var detailBody: String { missionDetails.body }
+    var category: String { missionDetails.category }
+    var mission: String { missionDetails.mission }
+
+    enum CodingKeys: String, CodingKey {
+        case id = "user_detail_id"
+        case userId = "user_id"
+        case createdAt = "created_at"
+        case missionDetails = "mission_details"
+    }
+}
